51aa8152b09f168905a58015fae8e44da16eb227
Анна Поворознюк <samadhihunter@gmail.com>
2022-06-08 13:06:49 +0700

    Спринт 6. Ревью 2: Протестированы модели Comment, Follow Исправлены ошибки по улучшению кода

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
 from http import HTTPStatus
+
+from django.shortcuts import render
 
 
 def page_not_found(request, exception):
     return render(request, 'core/404.html', {'path': request.path},
-                  HTTPStatus.NOT_FOUND)
+                  status=HTTPStatus.NOT_FOUND)
 
 
 def server_error(request):
