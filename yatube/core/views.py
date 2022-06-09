c2ae0c399ac1853770108fb4aa051c8248912eb2
Анна Поворознюк <samadhihunter@gmail.com>
2022-06-09 09:26:28 +0700

    Спринт 6. Ревью 2: Протестированы модели Comment, Follow Исправлены ошибки по улучшению кода

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -9,11 +9,12 @@ def page_not_found(request, exception):
 
 
 def server_error(request):
-    return render(request, 'core/500.html', status=500)
+    return render(request, 'core/500.html',
+                  status=HTTPStatus.INTERNAL_SERVER_ERROR)
 
 
 def permission_denied(request, exception):
-    return render(request, 'core/403.html', status=403)
+    return render(request, 'core/403.html', status=HTTPStatus.FORBIDDEN)
 
 
 def csrf_failure(request, reason=''):
